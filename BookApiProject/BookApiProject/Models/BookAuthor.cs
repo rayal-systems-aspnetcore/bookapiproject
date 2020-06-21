c6025f58908d07277eadc4b23396d29bcf844d0c
= <=>
2020-06-21 10:18:06 +0530

    Preparing the .Net Core Services, Models, and Database

---
+++ BookApiProject/BookApiProject/Models/BookAuthor.cs
@@ -0,0 +1,9 @@
+﻿namespace BookApiProject.Models {
+    public class BookAuthor {
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ BookApiProject/BookApiProject/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookApiProject.Models {
+    public class Category {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(50, ErrorMessage = "Category Name cannot be more than 50 characters.")]
+        public string Name { get; set; }
+
+        public virtual ICollection<BookCategory> BookCategories { get; set; }
+    }
+}
